e83679a9f2eb9bccb0de930c7208869adbbb6883
rick <LinuxSuRen@users.noreply.github.com>
2022-12-02 12:45:59 +0800

    feat: add invade mode support

--- k8s/controllers/invade_controller.go
+++ k8s/controllers/invade_controller.go
@@ -18,29 +18,29 @@ package controllers
 
 import (
 	"context"
-	"fmt"
 	"os"
 	"strings"
 
 	"github.com/go-logr/logr"
 	"gopkg.in/yaml.v3"
-	v1 "k8s.io/api/core/v1"
+	appsv1 "k8s.io/api/apps/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
-// MirrorReconciler reconciles a Mirror object
-type MirrorReconciler struct {
+// InvadeReconciler reconciles a Mirror object
+type InvadeReconciler struct {
 	client.Client
 	Log            logr.Logger
 	Scheme         *runtime.Scheme
 	ConfigFilepath string
 }
 
-const filter = "mirrors"
-
 //+kubebuilder:rbac:groups="",resources=pods,verbs=get;list;watch;create
+//+kubebuilder:rbac:groups="apps",resources=deployments,verbs=get;list;watch;update
+//+kubebuilder:rbac:groups="apps",resources=statefulsets,verbs=get;list;watch;update
+//+kubebuilder:rbac:groups="apps",resources=daemonsets,verbs=get;list;watch;update
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
@@ -51,7 +51,7 @@ const filter = "mirrors"
 //
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.6.4/pkg/reconcile
-func (r *MirrorReconciler) Reconcile(cxt context.Context, req ctrl.Request) (result ctrl.Result, err error) {
+func (r *InvadeReconciler) Reconcile(cxt context.Context, req ctrl.Request) (result ctrl.Result, err error) {
 	_ = r.Log.WithValues("mirror", req.NamespacedName)
 
 	// load mirror config items
@@ -60,102 +60,36 @@ func (r *MirrorReconciler) Reconcile(cxt context.Context, req ctrl.Request) (res
 		return
 	}
 
-	pod := &v1.Pod{}
-	if err = r.Get(cxt, req.NamespacedName, pod); err != nil {
+	deploy := &appsv1.Deployment{}
+	if err = r.Get(cxt, req.NamespacedName, deploy); err != nil {
 		err = client.IgnoreNotFound(err)
 		return
 	}
-	if pod.Status.Phase == v1.PodSucceeded {
-		return
-	}
-	if _, ok := pod.Labels[filter]; ok {
-		return
-	}
 
-	containers := pod.Spec.Containers
-	for _, container := range containers {
-		if strings.Contains(container.Image, "@") {
-			// cannot tag an image with digest
+	containers := deploy.Spec.Template.Spec.Containers
+	needUpdate := false
+	for i, container := range containers {
+		if !strings.Contains(container.Image, "@") {
+			// only take care of the image with digest
 			continue
 		}
 
-		var newImg string
-		skip := true
 		for key, item := range items {
 			if strings.HasPrefix(container.Image, key) {
-				newImg = strings.ReplaceAll(container.Image, key, item)
-				skip = false
+				newImg := strings.ReplaceAll(container.Image, key, item)
+				deploy.Spec.Template.Spec.Containers[i].Image = newImg
+				needUpdate = true
 				break
 			}
 		}
-		if skip {
-			continue
-		}
-
-		if r.isPulling(cxt, getID(pod.Spec.NodeName, newImg)) {
-			continue
-		}
-
-		newPod := &v1.Pod{}
-		newPod.Labels = map[string]string{
-			filter: getID(pod.Spec.NodeName, newImg),
-		}
-		newPod.GenerateName = pod.Name
-		newPod.Namespace = pod.Namespace
-		newPod.Spec.InitContainers = []v1.Container{{
-			Name:    "cache",
-			Image:   "docker.io/docker:20.10.21-alpine3.16",
-			Command: []string{"docker", "pull", newImg},
-			VolumeMounts: []v1.VolumeMount{{
-				Name:      "sock",
-				MountPath: "/var/run/docker.sock",
-			}},
-		}}
-		newPod.Spec.RestartPolicy = v1.RestartPolicyOnFailure
-		newPod.Spec.Volumes = []v1.Volume{{
-			Name: "sock",
-			VolumeSource: v1.VolumeSource{
-				HostPath: &v1.HostPathVolumeSource{
-					Path: "/var/run/docker.sock",
-				},
-			},
-		}}
-		newPod.Spec.Containers = []v1.Container{{
-			Name:    "tag",
-			Image:   "docker.io/docker:20.10.21-alpine3.16",
-			Command: []string{"docker", "tag", newImg, container.Image},
-			VolumeMounts: []v1.VolumeMount{{
-				Name:      "sock",
-				MountPath: "/var/run/docker.sock",
-			}},
-		}}
-		newPod.Spec.NodeName = pod.Spec.NodeName
-		if err := r.Create(cxt, newPod); err != nil {
-			fmt.Println("failed to create pod", err)
-		}
+	}
+	if needUpdate {
+		err = r.Update(cxt, deploy)
 	}
 	return
 }
 
-func getID(nodeName, image string) (result string) {
-	result = fmt.Sprintf("%s.%s", nodeName, image)
-	result = strings.ReplaceAll(result, "/", "")
-	result = strings.ReplaceAll(result, "@", "")
-	result = strings.ReplaceAll(result, ":", "")
-	return
-}
-
-func (r *MirrorReconciler) isPulling(ctx context.Context, name string) (ok bool) {
-	podList := &v1.PodList{}
-	if err := r.List(ctx, podList, client.MatchingLabels{
-		filter: name,
-	}); err == nil {
-		ok = len(podList.Items) > 0
-	}
-	return
-}
-
-func (r *MirrorReconciler) loadConfigItems() (items map[string]string, err error) {
+func (r *InvadeReconciler) loadConfigItems() (items map[string]string, err error) {
 	items = map[string]string{
 		"gcr.io/tekton-releases/github.com/tektoncd/pipeline/cmd/controller": "gcriotekton/pipeline-controller",
 		"gcr.io/tekton-releases/github.com/tektoncd/pipeline/cmd/webhook":    "gcriotekton/pipeline-webhook",
@@ -176,8 +110,8 @@ func (r *MirrorReconciler) loadConfigItems() (items map[string]string, err error
 }
 
 // SetupWithManager sets up the controller with the Manager.
-func (r *MirrorReconciler) SetupWithManager(mgr ctrl.Manager) error {
+func (r *InvadeReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
-		For(&v1.Pod{}).
+		For(&appsv1.Deployment{}).
 		Complete(r)
 }
